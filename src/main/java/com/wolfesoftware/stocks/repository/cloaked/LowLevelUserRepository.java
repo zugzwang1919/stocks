bf12c4e4a28300efa4949fe2ddaf5dc57793c27b
zugzwang1919 <russell.wolfe@gmail.com>
2021-05-01 15:33:26 -0400

    Finish up profile functionality.

--- src/main/java/com/wolfesoftware/stocks/repository/cloaked/LowLevelUserRepository.java
+++ src/main/java/com/wolfesoftware/stocks/repository/cloaked/LowLevelUserRepository.java
@@ -3,13 +3,15 @@ package com.wolfesoftware.stocks.repository.cloaked;
 import com.wolfesoftware.stocks.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.NonNullApi;
 
 import java.util.Optional;
 
 public interface LowLevelUserRepository extends JpaRepository<User, Long> {
 
     @Query("SELECT u FROM User u LEFT JOIN FETCH u.authorities a WHERE u.id = :id ")
-    Optional<User> findById(Long id);
+    Optional<User> findById( Long id);
 
     @Query("SELECT u FROM User u LEFT JOIN FETCH u.authorities a WHERE u.username = :username ")
     Optional<User> findByUsername(String username);
@@ -20,6 +22,7 @@ public interface LowLevelUserRepository extends JpaRepository<User, Long> {
 
 
     boolean existsByUsername(String username);
+
 
     <U extends User> U save(U user);
 
